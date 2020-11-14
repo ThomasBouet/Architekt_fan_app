d173f3c6a4fa42de783b763dc94432dc645732fc
ThomasBouet <thomasbouet@hotmail.fr>
2020-11-14 12:07:04 +0100

    fixing le scroll avec les boutons

--- Scripts/Competence.gd
+++ Scripts/Competence.gd
@@ -11,7 +11,7 @@ func _ready():
 	pass # Replace with function body.
 
 func init(comp):
-	get_node("LinkButton/Nom").text = comp["Nom"]
+	get_node("TextureButton/Nom").text = comp["Nom"]
 	get_node("Description").bbcode_text = comp["Description"]
 	return self
 	
@@ -26,3 +26,18 @@ func _on_LinkButton_pressed():
 	get_node("Description").visible = Description_visible
 	resize_self()
 
+#var pressdown_position = Vector2()
+#var press_threshold = 20
+#func _on_LinkButton_gui_input(event):
+#	if event is InputEventMouseButton : #with InputEventTouch it didn't want to work well
+#		print(event.position.distance_to(pressdown_position))
+#		if event.pressed:
+#			pressdown_position = event.position
+#		elif event.position.distance_to(pressdown_position) <= press_threshold:
+#			on_LinkButton_pressed() #call the function you want the buttonpress to call
+
+
+func _on_TextureButton_pressed():
+	Description_visible = !Description_visible
+	get_node("Description").visible = Description_visible
+	resize_self()
